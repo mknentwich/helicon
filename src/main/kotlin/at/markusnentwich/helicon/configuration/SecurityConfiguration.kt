8b69cb553168d410b252bcf247b3c5830c72610d
Richard Stöckl <richard.stoeckl@aon.at>
2020-09-26 14:55:24 +0200

    Disable the default spring login popup

---
+++ src/main/kotlin/at/markusnentwich/helicon/configuration/SecurityConfiguration.kt
@@ -0,0 +1,15 @@
+package at.markusnentwich.helicon.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http?.httpBasic()?.disable()
+    }
+}
