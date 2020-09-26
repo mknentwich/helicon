5b7e8e8e5a76404b752b0b69b49643d00a13be4b
Richard Stöckl <richard.stoeckl@aon.at>
2020-09-26 14:52:06 +0200

    Add the depency for swagger

--- src/main/kotlin/at/markusnentwich/helicon/HeliconApplication.kt
+++ src/main/kotlin/at/markusnentwich/helicon/HeliconApplication.kt
@@ -1,9 +1,23 @@
 package at.markusnentwich.helicon
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = Info(
+                title = "Helicon API",
+                version = "1.0",
+                description = "This is the backend API for Markus Nentwich",
+                license = License(name = "GNU Free Documentation License", url = "https://www.gnu.org/licenses/fdl-1.3.html"),
+                contact = Contact(url = "https://github.com/Eiskasten", name = "Richard Stöckl", email = "richard.stoeckl@aon.at")
+        )
+)
 class HeliconApplication
 
 fun main(args: Array<String>) {
