f6d9439a572f5f55aa1021e9987e0a487861160f
Richard Stöckl <richard.stoeckl@aon.at>
2020-09-27 11:41:38 +0200

    Add order confirmation

--- src/main/kotlin/at/markusnentwich/helicon/dto/ScoreOrderDto.kt
+++ src/main/kotlin/at/markusnentwich/helicon/dto/ScoreOrderDto.kt
@@ -9,16 +9,26 @@ data class ScoreOrderDto(
         val id: Int?,
         @Schema(description = "the address for the bill. this may be null when a registered user orders something, if so the users address will be used")
         val billingAddress: AddressDto?,
+        @Schema(description = "the number of the bill. this is only used during confirmation", example = "2020092701")
+        val billingNumber: String?,
         @Schema(description = "the receiving company or society of this order", example = "Musikverein Leopoldsdorf")
         val company: String?,
+        @Schema(description = "the date when the order was confirmed from the user")
+        val confirmed: LocalDateTime?,
         @Schema(description = "the delivery address. this may be null, if so, the billingAddress will be used")
         val deliveryAddress: AddressDto?,
         @Schema(description = "the identity of this order")
         val identity: IdentityDto,
+        @Schema(description = "the date since when the order is in progress")
+        val inProgress: LocalDateTime?,
         @Schema(description = "the shopping cart, this is only used when committing an order. the key is the score id and the value the amount")
         val cart: Map<Int, Int>?,
         @Schema(description = "the items which belong to this order, this will only be available after ordering. the key is the score id and the value the amount")
         val items: Map<ScoreProductDto, Int>?,
+        @Schema(description = "the price of the order in subunits, e.g. cent. this is only during confirmation", example = "3995")
+        val price: Int?,
         @Schema(description = "the date when the backend received the order. will be null while ordering")
-        val receivedOn: LocalDateTime?
+        val receivedOn: LocalDateTime?,
+        @Schema(description = "the date when the order was sent")
+        val sent: LocalDateTime?
 )
--- src/main/kotlin/at/markusnentwich/helicon/services/OrderService.kt
+++ src/main/kotlin/at/markusnentwich/helicon/services/OrderService.kt
@@ -2,9 +2,13 @@ package at.markusnentwich.helicon.services
 
 import at.markusnentwich.helicon.dto.ScoreOrderDto
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -19,8 +23,21 @@ interface OrderService {
     @Operation(description = "Perform an order. if the identity is embedded, the state only has to be provided via its id. the same applies to the scores. the attribute items must not be provided, cart should be used instead as items is only used for get requests.")
     @ApiResponses(
             value = [
-                ApiResponse(responseCode = "200", description = "OK")
+                ApiResponse(responseCode = "200", description = OK)
             ]
     )
-    fun order(@RequestBody order: ScoreOrderDto)
+    fun order(@RequestBody order: ScoreOrderDto): ResponseEntity<ScoreOrderDto>
+
+    @RequestMapping("/confirm/{id}", method = [RequestMethod.PUT])
+    @Operation(description = "confirm an order. if the order is done by a registered user, the user must be authenticated for that")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = OK, content = [Content(schema = Schema(implementation = ScoreOrderDto::class))]),
+                ApiResponse(responseCode = "400", description = BAD_REQUEST),
+                ApiResponse(responseCode = "401", description = UNAUTHORIZED),
+                ApiResponse(responseCode = "403", description = FORBIDDEN),
+                ApiResponse(responseCode = "404", description = "no order found with such id")
+            ]
+    )
+    fun confirm(@Parameter(description = "the id of the order") @PathVariable id: Int): ResponseEntity<ScoreOrderDto>
 }
