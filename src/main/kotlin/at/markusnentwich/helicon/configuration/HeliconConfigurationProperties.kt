c3efe5a45f28f1906b226f7aa33903b53ed6fa3a
Richard Stöckl <richard.stoeckl@aon.at>
2020-10-11 18:10:31 +0200

    Add bank properties

--- src/main/kotlin/at/markusnentwich/helicon/configuration/HeliconConfigurationProperties.kt
+++ src/main/kotlin/at/markusnentwich/helicon/configuration/HeliconConfigurationProperties.kt
@@ -14,7 +14,9 @@ data class HeliconConfigurationProperties(
     /** Mail configuration. */
     val mail: MailConfiguration = MailConfiguration(),
     /** Path for the assets such as templates, score samples. */
-    val assets: String = "assets"
+    val assets: String = "assets",
+    /** The bank account for bills.     */
+    val bank: BankConfiguration = BankConfiguration()
 ) {
     data class LoginConfiguration(
         /** Enable user logins, this does not apply to administration accounts. */
@@ -47,4 +49,13 @@ data class HeliconConfigurationProperties(
             val registration: Boolean = true
         )
     }
+
+    data class BankConfiguration(
+        /** The iban of the owners bank account. */
+        val iban: String = "AT00 0000 0000 0000 0000",
+        /** The bic of the owners bank account. */
+        val bic: String = "RLNWATZUDJ",
+        /** The name of the owners bank */
+        val name: String = "Raika"
+    )
 }
