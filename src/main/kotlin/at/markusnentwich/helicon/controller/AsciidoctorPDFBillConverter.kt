319dd5be9baa9adc7c992c34e68f61ed4753978a
Dennis Toth <dennis.toth@gmx.at>
2020-12-22 19:38:23 +0100

    adapted createBill, changed return type to a stream (i hope this helps)

--- src/main/kotlin/at/markusnentwich/helicon/controller/AsciidoctorPDFBillConverter.kt
+++ src/main/kotlin/at/markusnentwich/helicon/controller/AsciidoctorPDFBillConverter.kt
@@ -1,13 +1,13 @@
 package at.markusnentwich.helicon.controller
 
 import at.markusnentwich.helicon.configuration.HeliconConfigurationProperties
-import at.markusnentwich.helicon.entities.IdentityEntity
 import at.markusnentwich.helicon.entities.OrderEntity
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.AttributesBuilder
 import org.asciidoctor.OptionsBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import java.io.ByteArrayOutputStream
 import java.io.File
 
 @Controller
@@ -16,38 +16,43 @@ class AsciidoctorPDFBillConverter(
     @Autowired val config: HeliconConfigurationProperties
 ) : BillConverter {
 
-    override fun createBill(order: OrderEntity, owner: IdentityEntity): Array<Byte> {
-        val o = OptionsBuilder.options().backend("pdf").attributes(
-            AttributesBuilder
-                .attributes("bill.orders=${ordersAsCSV(order)}")
-                .attribute("bill.order", ordersAsCSV(order))
-                .attribute("bill.number")
-                .attribute("bill.date")
-                .attribute("bill.total", order.total())
-                .attribute("owner.firstname", owner.firstName)
-                .attribute("owner.lastname", owner.lastName)
-                .attribute("owner.street", owner.address.street)
-                .attribute("owner.streetnumber", owner.address.streetNumber)
-                .attribute("owner.postcode", owner.address.postCode)
-                .attribute("owner.city", owner.address.city)
-                .attribute("owner.telephone", owner.telephone)
-                .attribute("owner.email", owner.email)
-                .attribute("owner.website", config.domain)
-                .attribute("customer.company", order.identity.company)
-                .attribute("customer.firstname", order.identity.firstName)
-                .attribute("customer.lastname", order.identity.lastName)
-                .attribute("customer.street", order.identity.address.street)
-                .attribute("customer.streetnumber", order.identity.address.streetNumber)
-                .attribute("customer.postcode", order.identity.address.postCode)
-                .attribute("customer.city", order.identity.address.city)
-                .attribute("customer.state", order.identity.address.state.name)
-                .attribute("bank.bic", config.bank.bic)
-                .attribute("bank.iban", config.bank.iban)
-                .attribute("bank.name", config.bank.name)
-                .attribute("bank.reference")
-        )
-        asciidoctor.loadFile(File(""), o.asMap())
-        return arrayOf()
+    override fun createBill(order: OrderEntity): ByteArrayOutputStream {
+        val baos = ByteArrayOutputStream();
+        val options = OptionsBuilder.options()
+            .backend("pdf")
+            .toStream(baos)
+            .attributes(
+                AttributesBuilder.attributes("bill.orders=${ordersAsCSV(order)}")
+                    .attribute("pdf-themesdir", "src/main/resources/assets/bill/themes")
+                    .attribute("pdf-theme", "mknen-theme.yml")
+                    .attribute("bill.order", ordersAsCSV(order))
+                    .attribute("bill.number")
+                    .attribute("bill.date")
+                    .attribute("bill.total", order.total())
+                    .attribute("owner.name", config.bill.address.name)
+                    .attribute("owner.street", config.bill.address.street)
+                    .attribute("owner.streetNumber", config.bill.address.streetNumber)
+                    .attribute("owner.postCode", config.bill.address.postCode)
+                    .attribute("owner.city", config.bill.address.city)
+                    .attribute("owner.phone", config.bill.address.phone)
+                    .attribute("owner.email", config.bill.address.mail)
+                    .attribute("owner.website", config.domain)
+                    .attribute("customer.company", order.identity.company)
+                    .attribute("customer.firstname", order.identity.firstName)
+                    .attribute("customer.lastname", order.identity.lastName)
+                    .attribute("customer.street", order.identity.address.street)
+                    .attribute("customer.streetnumber", order.identity.address.streetNumber)
+                    .attribute("customer.postcode", order.identity.address.postCode)
+                    .attribute("customer.city", order.identity.address.city)
+                    .attribute("customer.state", order.identity.address.state.name)
+                    .attribute("bank.name", config.bill.bank.name)
+                    .attribute("bank.bic", config.bill.bank.bic)
+                    .attribute("bank.iban", config.bill.bank.iban)
+                    .attribute("bank.institute", config.bill.bank.institute)
+                    .attribute("bank.reference").get()
+            ).get()
+        asciidoctor.convertFile(File("src/main/resources/assets/bill/bill.adoc"), options)
+        return baos
     }
 
     private fun ordersAsCSV(order: OrderEntity): String {
--- src/main/kotlin/at/markusnentwich/helicon/controller/BillController.kt
+++ src/main/kotlin/at/markusnentwich/helicon/controller/BillController.kt
@@ -1,9 +1,10 @@
 package at.markusnentwich.helicon.controller
 
 import org.springframework.stereotype.Controller
+import java.io.ByteArrayOutputStream
 import java.util.UUID
 
 @Controller
 interface BillController {
-    fun bill(id: UUID, jwt: String): Array<Byte>
+    fun bill(id: UUID, jwt: String): ByteArrayOutputStream
 }
