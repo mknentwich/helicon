80dc1146e8d9153fee48ed258a11d88137e657fe
Richard Stöckl <richard.stoeckl@aon.at>
2020-12-28 19:09:18 +0100

    Add anonymous constructor to ScoreProductOrder

--- src/main/kotlin/at/markusnentwich/helicon/dto/ScoreProductDto.kt
+++ src/main/kotlin/at/markusnentwich/helicon/dto/ScoreProductDto.kt
@@ -5,17 +5,17 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(name = "Score")
 data class ScoreProductDto(
     @Schema(description = "the id of this score", example = "73")
-    var id: Long?,
+    var id: Long? = null,
     @Schema(description = "the category of this score")
-    var category: CategoryProductDto?,
+    var category: CategoryProductDto? = null,
     @Schema(description = "the difficulty of this score", example = "2", minimum = "1", maximum = "5")
-    var difficulty: Int,
+    var difficulty: Int = 2,
     @Schema(description = "the instrumentation of this score", example = "1. Klarinette in B, 2. Klarinette in B")
-    var instrumentation: String,
+    var instrumentation: String = "Posaunen",
     @Schema(description = "the price of this score represented in subunits (e.g. cent)", example = "3995")
-    var price: Int?,
+    var price: Int? = 3995,
     @Schema(description = "the quantity of this score. only used during an order. during an order not null", example = "2")
-    var quantity: Int?,
-    @Schema(description = "the title of this score")
-    var title: String?,
+    var quantity: Int? = null,
+    @Schema(description = "the title of this score", example = "Eine letzte Runde")
+    var title: String? = "Eine letzte Runde",
 )
