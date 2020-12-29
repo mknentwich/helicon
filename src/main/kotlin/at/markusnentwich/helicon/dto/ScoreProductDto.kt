68bbd02315490a08f4786cddf433c6b50dcb2f40
Richard Stöckl <richard.stoeckl@aon.at>
2020-12-29 13:46:34 +0100

    Add author and grouptype

--- src/main/kotlin/at/markusnentwich/helicon/dto/ScoreProductDto.kt
+++ src/main/kotlin/at/markusnentwich/helicon/dto/ScoreProductDto.kt
@@ -6,10 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class ScoreProductDto(
     @Schema(description = "the id of this score", example = "73")
     var id: Long? = null,
+    @Schema(description = "author of this score", example = "Markus Nentwich")
+    var author: String = "Markus Nentwich",
     @Schema(description = "the category of this score")
     var category: CategoryProductDto? = null,
     @Schema(description = "the difficulty of this score", example = "2", minimum = "1", maximum = "5")
     var difficulty: Int = 2,
+    @Schema(description = "the group type of this score", example = "Blasorchester")
+    var groupType: String = "Blasorchester",
     @Schema(description = "the instrumentation of this score", example = "1. Klarinette in B, 2. Klarinette in B")
     var instrumentation: String = "Posaunen",
     @Schema(description = "the price of this score represented in subunits (e.g. cent)", example = "3995")
