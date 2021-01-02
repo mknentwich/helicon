f5a15a615136a017e736547d2d34ab120f2d2207
oliver <oliver.petschk@gmail.com>
2021-01-02 18:53:34 +0100

    getRoles geändert

--- src/main/kotlin/at/markusnentwich/helicon/controller/AccountControllerImpl.kt
+++ src/main/kotlin/at/markusnentwich/helicon/controller/AccountControllerImpl.kt
@@ -6,9 +6,7 @@ import at.markusnentwich.helicon.entities.AccountEntity
 import at.markusnentwich.helicon.entities.CategoryEntity
 import at.markusnentwich.helicon.entities.ROOT_CATEGORY_NAME
 import at.markusnentwich.helicon.entities.ScoreEntity
-import at.markusnentwich.helicon.repositories.AccountRepository
-import at.markusnentwich.helicon.repositories.CategoryRepository
-import at.markusnentwich.helicon.repositories.ScoreRepository
+import at.markusnentwich.helicon.repositories.*
 import org.modelmapper.ModelMapper
 import org.modelmapper.TypeToken
 import org.slf4j.LoggerFactory
@@ -21,19 +19,20 @@ import java.lang.reflect.Type
 @Controller
 class AccountControllerImpl(
     @Autowired val config: HeliconConfigurationProperties,
-    @Autowired val accountRepo: AccountRepository,
+    @Autowired val accountRepository: AccountRepository,
+    @Autowired val rolesRepository: ZoneRepository,
     @Autowired val mapper: ModelMapper
 ) : AccountController {
 
     val logger = LoggerFactory.getLogger(OrderControllerImpl::class.java)
 
     override fun getUser(username: String): AccountDto{
-        if (!accountRepo.existsById(username)) {
+        if (!accountRepository.existsById(username)) {
             logger.error("order with id {} does not exist", username)
             throw NotFoundException()
         }
 
-        val entity = accountRepo.findById(username).get()
+        val entity = accountRepository.findById(username).get()
         val dto = mapper.map(entity, AccountDto::class.java)
         dto.identity = mapper.map(entity.identity, IdentityDto::class.java)
         dto.password = if (entity.password.isNullOrEmpty()) null else mapper.map(entity.password, String::class.java)
@@ -51,8 +50,8 @@ class AccountControllerImpl(
         if (accountEntity.username != null && accountEntity.password != null && accountEntity.identity != null) {
             throw BadPayloadException()
         }
-        var accountdto = accountRepo.findByIdOrNull(account.username)
-        val saved = accountRepo.save(accountdto)
+        var accountdto = accountRepository.findByIdOrNull(account.username)
+        val saved = accountRepository.save(accountdto)
         return ResponseEntity.accepted().body(account);
 
 
@@ -63,34 +62,26 @@ class AccountControllerImpl(
         if (accountEntity.username != null && accountEntity.password != null && accountEntity.identity != null) {
             throw BadPayloadException()
         }
-        val saved = accountRepo.save(accountEntity)
+        val saved = accountRepository.save(accountEntity)
         return ResponseEntity.ok(account)
     }
 
     override fun deleteAccount(account: String): ResponseEntity<Void>{
-        val accountEntity = accountRepo.findById(account).get() ?: throw NotFoundException()
+        val accountEntity = accountRepository.findById(account).get() ?: throw NotFoundException()
         if (accountEntity.identity != null ) {
             throw DependentEntriesException()
         }
-        accountRepo.deleteById(account)
+        accountRepository.deleteById(account)
         return ResponseEntity.ok().build()
     }
     override fun getRoles(): ResponseEntity<Iterable<RoleDto>>{
 
-        val entity = accountRepo.findById("username").get()
+        val listType: Type = object : TypeToken<List<RoleDto>>() {}.type
+        return mapper.map(rolesRepository.findAll(), listType)
 
-        Iterable<RoleDto> role =  object : TypeToken<List<AccountDto>>() {}.ty
-
-        HttpHeaders headers = new HttpHeaders();
-
-        return new ResponseEntity<Iterable<User>>(user, headers, HttpStatus.OK);
-
-
-        val listType: Type = object : TypeToken<List<ScoreProductDto>>() {}.type
-        return mapper.map(AccountRepository.(), listType)
     }
 
-    override fun register(user: AccountDto): ResponseEntity<AccountDto> {
+    override fun register(account: AccountDto): ResponseEntity<AccountDto> {
         TODO("Not yet implemented")
     }
 
