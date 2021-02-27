02de5e343f3a5006744f5769dbba50afa7cea4a7
Richard Stöckl <richard.stoeckl@aon.at>
2021-02-27 17:53:01 +0100

    Disable password serialization in jackson

--- src/main/kotlin/at/markusnentwich/helicon/dto/AccountDto.kt
+++ src/main/kotlin/at/markusnentwich/helicon/dto/AccountDto.kt
@@ -1,12 +1,14 @@
 package at.markusnentwich.helicon.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "Account")
 data class AccountDto(
     @Schema(description = "username of this account")
     var username: String = "nobody",
-    @Schema(description = "cleartext password of this account, will only be used during registration, login and password change")
+    @Schema(description = "cleartext password of this account, will only be used during registration, login and password change", writeOnly = true)
+    @field:JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     var password: String? = null,
     @Schema(description = "the identity of this account")
     var identity: IdentityDto = IdentityDto(),
